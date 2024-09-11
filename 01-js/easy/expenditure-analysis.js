0e7dd375e5642767487d4cf1cc18401ada80c4bf
CodingRun15 <154725027+CodingRun15@users.noreply.github.com>
2024-09-11 16:51:33 +0530

    easy

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -12,9 +12,77 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656163200000,
+    price: 15,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656249600000,
+    price: 7,
+    category: 'Beverage',
+    itemName: 'Coke',
+  },
+  {
+    id: 4,
+    timestamp: 1656336000000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Pasta',
+  },
+  {
+    id: 5,
+    timestamp: 1656422400000,
+    price: 5,
+    category: 'Snack',
+    itemName: 'Chips',
+  },
+  {
+    id: 6,
+    timestamp: 1656508800000,
+    price: 3,
+    category: 'Beverage',
+    itemName: 'Water',
+  },
+  {
+    id: 7,
+    timestamp: 1656595200000,
+    price: 8,
+    category: 'Snack',
+    itemName: 'Cookies',
+  },
+  {
+    id: 8,
+    timestamp: 1656681600000,
+    price: 12,
+    category: 'Food',
+    itemName: 'Sandwich',
+  }
+];
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+ let categoryTotals = {};
+ transactions.forEach(transaction=>{
+  if(!categoryTotals[transaction.category]) { 
+    categoryTotals[transaction.category] = 0;
+  }
+  categoryTotals[transaction.category] += transaction.price;
+})
+const result =  Object.keys(categoryTotals).map(key=>{
+  return {category: key, totalSpent: categoryTotals[key]};  // return array of objects
+});
+return result;
 }
 
-module.exports = calculateTotalSpentByCategory;
+console.log(calculateTotalSpentByCategory(transactions));
