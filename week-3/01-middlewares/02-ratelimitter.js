e9c14bf1a06ddc34df2cd7792c59f1169e8987ea
CodingRun15 <154725027+CodingRun15@users.noreply.github.com>
2024-11-30 15:33:37 +0530

    solved week3

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -15,7 +15,18 @@ let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
-
+app.use(function(req,res,next){
+  let userId = req.headers['userId'];
+  if(numberOfRequestsForUser[userId]){
+    if(numberOfRequestsForUser[userId]>=5){
+      res.status(404).send("too many requests. Not allowed");
+    }else{
+      numberOfRequestsForUser[userId]++;
+    }
+  }else{
+    numberOfRequestsForUser[userId] =1;
+  }
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
